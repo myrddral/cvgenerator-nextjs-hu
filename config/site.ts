9c567b9e1f343a0738458c9eedfb22b6181d0979
myrddral <thirdeye0@gmail.com>
2024-06-25 00:14:17 +0200

    added site cgf, fonts, utils | removed default page content

---
+++ config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "Önéletrajz Generátor",
+  description: "Generálj egyszerűen és gyorsan, ízlésesen formázott önéletrajzot!",
+  url: "https://onletrajz-generator.vercel.app",
+  ogImage:
+    "https://placehold.co/1200x630/030712/F9FAFB?text=%C3%96n%C3%A9letrajz\nGener%C3%A1tor&font=raleway",
+  locale: "hu_HU",
+  htmlLang: "hu"
+};
+
+export type SiteConfig = typeof siteConfig;
---
+++ lib/fonts.ts
@@ -0,0 +1,16 @@
+import { JetBrains_Mono as FontMono, Exo as FontSans, Advent_Pro as FontDisplay } from "next/font/google";
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans"
+});
+
+export const fontDisplay = FontDisplay({
+  subsets: ["latin"],
+  variable: "--font-display"
+});
+
+export const fontMono = FontMono({
+  subsets: ["latin"],
+  variable: "--font-mono"
+});
