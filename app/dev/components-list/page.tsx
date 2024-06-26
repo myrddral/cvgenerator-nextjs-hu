f0e052c6b0182b8a67cbb8ae0468cc7876804f17
myrddral <thirdeye0@gmail.com>
2024-06-26 05:10:13 +0200

    added button component + a page to list available button variants

---
+++ app/dev/components-list/page.tsx
@@ -0,0 +1,18 @@
+import ButtonsShowcase from "@/components/showcase/buttons-showcase"
+
+export default function ComponentsPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <section className="container">
+        <h1 className="mb-8 text-center font-display text-3xl font-bold leading-none tracking-wider">
+          Komponenslista
+        </h1>
+        <p className="text-center text-lg">Itt találod az összes komponenst, amit a projekt tartalmaz.</p>
+        <p className="text-center text-sm italic text-muted-foreground">(bővítés alatt)</p>
+      </section>
+      <section className="container gap-8 pt-8">
+        <ButtonsShowcase />
+      </section>
+    </main>
+  )
+}
---
+++ components/showcase/buttons-showcase.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+
+type ButtonVariants =
+  | "default"
+  | "destructive"
+  | "outline"
+  | "secondary"
+  | "ghost"
+  | "link"
+  | null
+  | undefined
+type ButtonSizes = "default" | "sm" | "lg" | "icon" | null | undefined
+
+const buttonVariants = ["default", "destructive", "outline", "secondary", "ghost", "link"]
+const buttonSizes = ["sm", "default", "lg", "icon"]
+
+export default function ButtonsShowcase() {
+  return (
+    <>
+      <h2 className="m-2 font-display text-lg font-semibold tracking-wider border-b-2">Buttons</h2>
+      <div className="flex flex-wrap gap-x-12 gap-y-8">
+        {buttonVariants.map((variant) => (
+          <div key={variant} className="p-2">
+            <h3 className="mb-2 text-sm font-semibold">variant: {variant}</h3>
+            <div className="flex items-center gap-2">
+              {buttonSizes.map((size) => (
+                <Button key={size} variant={variant as ButtonVariants} size={size as ButtonSizes}>
+                  {size.toUpperCase()}
+                </Button>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
