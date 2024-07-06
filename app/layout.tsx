f5e14762b614908ac17947949edb10d6bc8f16cd
myrddral <thirdeye0@gmail.com>
2024-07-06 04:07:02 +0200

    added packages for speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import type { PropsWithChildren } from "react"
 
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Footer from "@/components/footer"
 import Navbar from "@/components/navbar"
 import { CvDataStoreProvider } from "@/components/providers/cvdata-store-provider"
@@ -75,6 +76,7 @@ export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
           </ThemeProvider>
         </CvDataStoreProvider>
       </body>
+      <SpeedInsights />
     </html>
   )
 }
