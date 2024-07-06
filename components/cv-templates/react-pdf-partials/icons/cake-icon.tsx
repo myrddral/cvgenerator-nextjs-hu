6d7bbcb8b86adf8a27dbc87d57cdd9a8f3577ea4
myrddral <thirdeye0@gmail.com>
2024-07-06 03:14:12 +0200

    implemented SVG based icon components

---
+++ components/cv-templates/react-pdf-partials/icons/cake-icon.tsx
@@ -0,0 +1,19 @@
+import type { SvgIconProps } from "../svg-icon"
+import { Path } from "@react-pdf/renderer"
+import { SvgIcon } from "../svg-icon"
+
+export const CakeIcon = ({}: SvgIconProps) => {
+  return (
+    <SvgIcon viewBox="1 3 21 22" strokeWidth={1.9} style={{ position: "relative", top: 0.5 }}>
+      <Path d="M20 21v-8a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v8" />
+      <Path d="M4 16s.5-1 2-1 2.5 2 4 2 2.5-2 4-2 2.5 2 4 2 2-1 2-1" />
+      <Path d="M2 21h20" />
+      <Path d="M7 8v3" />
+      <Path d="M12 8v3" />
+      <Path d="M17 8v3" />
+      <Path d="M7 4h.01" />
+      <Path d="M12 4h.01" />
+      <Path d="M17 4h.01" />
+    </SvgIcon>
+  )
+}
---
+++ components/cv-templates/react-pdf-partials/icons/mail-icon.tsx
@@ -0,0 +1,13 @@
+import type { SvgIconProps } from "../svg-icon"
+import { Path, Rect } from "@react-pdf/renderer"
+import { SvgIcon } from "../svg-icon"
+import { colors } from "../../template001"
+
+export const MailIcon = ({}: SvgIconProps) => {
+  return (
+    <SvgIcon strokeWidth={1.9} style={{ position: "relative", top: 2.6 }}>
+      <Rect width="20" height="16" x="2" y="4" rx="2" ry="2" />
+      <Path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+    </SvgIcon>
+  )
+}
