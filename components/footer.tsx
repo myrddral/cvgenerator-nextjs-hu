4e685ecb8eb4b9e9a46cad2c4cc4175d113aecc6
myrddral <thirdeye0@gmail.com>
2024-07-16 01:48:02 +0200

    fixed /terms footer link

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="mb-2 flex flex-wrap justify-center gap-5 text-sm">
         <Link href="https://www.attilabeli.com">Készítette</Link>
         <Link href="/privacy">Adatvédelmi szabályzat</Link>
-        <Link href="/aszf">Használati feltételek</Link>
+        <Link href="/terms">Használati feltételek</Link>
         <Link href="/attribution">Kredit</Link>
       </div>
       <p className="text-center text-sm text-gray-500">{new Date().getFullYear()} | Attila Béli</p>
