d0550b2b73ca9b8ae7b7b13a158b9f2e26d95c44
myrddral <thirdeye0@gmail.com>
2024-07-20 09:10:49 +0200

    test snapshot updated + form card width fixed

--- app/create/template.tsx
+++ app/create/template.tsx
@@ -7,7 +7,7 @@ export default function CreateTemplate({ children }: PropsWithChildren) {
   return (
     <div className="flex w-full flex-1 flex-col items-center overflow-clip">
       <Stepper allSections={allSections} />
-      <div className="flex flex-1 flex-col items-center justify-center">{children}</div>
+      <div className="flex w-full max-w-screen-md flex-1 flex-col items-center justify-center">{children}</div>
     </div>
   )
 }
