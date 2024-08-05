8a255bf2a498124ce99cb4f9281f4dbdfdea2317
myrddral <thirdeye0@gmail.com>
2024-08-05 13:21:33 +0200

    added date-fns formatter to utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,7 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { format } from "date-fns"
+import { hu } from "date-fns/locale"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -7,4 +9,8 @@ export function cn(...inputs: ClassValue[]) {
 
 export function isDevMode(): boolean {
   return process.env.NODE_ENV === "development"
+}
+
+export const formatDate = (date: string | Date) => {
+  return format(date, "yyyy. MMMM", { locale: hu })
 }
