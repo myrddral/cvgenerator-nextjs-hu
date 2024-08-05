22d606dd0536bad835f3f7833628e2d145344837
myrddral <thirdeye0@gmail.com>
2024-08-05 13:14:50 +0200

    added nav button variants to button component | replaced start button on starter card with new nav button variant

--- components/starter-card.tsx
+++ components/starter-card.tsx
@@ -1,47 +1,28 @@
-"use client"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
-import { cn } from "@/lib/utils"
-import { ChevronRightIcon } from "@radix-ui/react-icons"
 import Link from "next/link"
-import { useState } from "react"
 import Logo from "./ui/logo"
 
 export default function StarterCard() {
-  const [isHelpVisible, setIsHelpVisible] = useState(false)
-  const [isHovered, setIsHovered] = useState(false)
-
   return (
     <Card className="max-w-96">
       <CardContent className="flex flex-col items-center p-20 max-sm:p-16">
-        {!isHelpVisible ? (
-          <>
-            <Logo className="w-56" />
-            <h3 className="mt-2 font-semibold">Önéletrajz generátor</h3>
-            <Link href="/create">
-              <Button
-                size="lg"
-                className="relative mt-12"
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-              >
-                KEZDÉS
-                <ChevronRightIcon
-                  className={cn(
-                    "absolute right-8 translate-x-3.5",
-                    isHovered
-                      ? "scale-125 opacity-100 duration-500 animate-in fade-in-0 slide-in-from-left"
-                      : "opacity-0"
-                  )}
-                />
-              </Button>
-            </Link>
-            <Button size="lg" variant={"ghost"} className="mt-2" onClick={() => setIsHelpVisible(true)}>
+        <>
+          <Logo className="w-56" />
+          <h3 className="mt-2 font-semibold">Önéletrajz generátor</h3>
+          <Link href="/create">
+            <Button size="lg" variant="navNext" className="group relative mt-12">
+              KEZDÉS
+            </Button>
+          </Link>
+          <Link href="/create">
+            <Button size="lg" variant={"ghost"} className="mt-2">
               Mi ez?
             </Button>
-          </>
-        ) : (
-          <>
+          </Link>
+        </>
+
+        {/* <>
             <p className="text-center text-lg">
               Ez egy fejlesztés alatt álló webalkalmazás, mellyel kényelmesen készíthetsz profi önéletrajzot,
               majd letöltheted azt PDF formátumban.
@@ -49,8 +30,7 @@ export default function StarterCard() {
             <Button size="lg" variant={"ghost"} className="mt-8" onClick={() => setIsHelpVisible(false)}>
               Vissza
             </Button>
-          </>
-        )}
+          </> */}
       </CardContent>
     </Card>
   )
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils"
 import { Slot } from "@radix-ui/react-slot"
 import { cva } from "class-variance-authority"
 import { forwardRef } from "react"
+import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium transition-colors hover:brightness-110 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
@@ -17,7 +18,10 @@ const buttonVariants = cva(
         secondary: "bg-secondary text-secondary-foreground shadow-sm",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        picker: "border border-input bg-transparent/30 text-primary-foreground shadow-sm hover:text-accent-foreground hover:border-ring",
+        picker:
+          "border border-input bg-transparent/30 text-primary-foreground shadow-sm hover:border-ring hover:text-accent-foreground",
+        navPrev: "relative bg-primary text-primary-foreground shadow-black/30 !text-shadow-sm",
+        navNext: "relative bg-primary text-primary-foreground shadow-black/30 !text-shadow-sm",
       },
       size: {
         default: "h-9 min-w-28 px-4 py-2 text-sm",
@@ -42,7 +46,17 @@ export interface ButtonProps
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
-    return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+    return (
+      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props}>
+        {variant === "navPrev" && (
+          <ChevronLeftIcon className="absolute left-8 transform opacity-0 transition-transform duration-300 ease-out group-hover:translate-x-[-0.875rem] group-hover:opacity-100" />
+        )}
+        {props.children}
+        {variant === "navNext" && (
+          <ChevronRightIcon className="absolute right-8 transform opacity-0 transition-transform duration-300 ease-out group-hover:translate-x-3.5 group-hover:opacity-100" />
+        )}
+      </Comp>
+    )
   }
 )
 Button.displayName = "Button"
