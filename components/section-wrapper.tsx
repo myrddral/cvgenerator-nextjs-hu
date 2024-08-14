80771c77b36c2b8a1ca26fb339e78e2d63ab4632
myrddral <thirdeye0@gmail.com>
2024-08-14 18:09:13 +0200

    refactor: wrapped form generator with lifted state for better state mgmt and navigation

---
+++ components/section-wrapper.tsx
@@ -0,0 +1,97 @@
+"use client"
+import type { SectionProps } from "@/form-generator/generator-sections"
+import type {
+  CvDataState,
+  Employment,
+  Language,
+  School,
+  SectionName,
+  SectionNameWithMultiEntry,
+} from "@/lib/stores/cv-data-store.types"
+
+import { SectionDataList } from "@/components/section-data-list"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { FormStepCard } from "@/components/ui/formstep-card"
+import { IconButton } from "@/components/ui/iconbutton"
+import FormGenerator from "@/form-generator/form-generator"
+import { PlusIcon } from "@radix-ui/react-icons"
+import { useState } from "react"
+import { useCvDataStore } from "./providers/cv-data-store-provider"
+import { useFormNavigation } from "@/hooks/use-form-navigation"
+import { FormNavButtons } from "./form-nav-buttons"
+
+export interface FormStepWrapperProps extends SectionProps {}
+
+export function SectionWrapper({ ...sectionProps }: FormStepWrapperProps) {
+  const { title, sub, sectionName, isMultiEntry } = sectionProps
+  const { setSectionData, addToList, removeFromList } = useCvDataStore((state) => state)
+  const sectionData = useCvDataStore((state) => state)[sectionName]
+  const { handleForwardStep } = useFormNavigation(sectionName)
+  const [isOpen, setIsOpen] = useState(false)
+  const [selectedListItem, setSelectedListItem] = useState<number | undefined>(undefined)
+
+  function onSubmit(data: Omit<CvDataState[SectionName], "email">) {
+    if (isMultiEntry) {
+      addToList(sectionName, data as Employment | School | Language)
+      setIsOpen(false)
+    } else {
+      setSectionData(sectionName, data)
+      handleForwardStep()
+    }
+  }
+
+  function onDelete() {
+    if (isMultiEntry && selectedListItem !== undefined) {
+      removeFromList(sectionName as SectionNameWithMultiEntry, selectedListItem)
+      setIsOpen(false)
+    }
+  }
+
+  function getValues() {
+    if (isMultiEntry && Array.isArray(sectionData)) {
+      return sectionData[selectedListItem ?? 0]
+    }
+    return sectionData
+  }
+
+  function handleOnOpenChange(isOpen: boolean) {
+    setIsOpen(isOpen)
+    if (!isOpen) setSelectedListItem(undefined)
+  }
+
+  return (
+    <FormStepCard title={title} sub={sub} className="mb-8">
+      {isMultiEntry ? (
+        <>
+          <Dialog open={isOpen} onOpenChange={handleOnOpenChange}>
+            <DialogTrigger asChild className="mb-6">
+              <IconButton icon={<PlusIcon />} text="Új felvétele" />
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader className="mb-2">
+                <DialogTitle>Új munkatapasztalat hozzáadása</DialogTitle>
+                <DialogDescription></DialogDescription>
+              </DialogHeader>
+              <FormGenerator {...sectionProps} values={getValues()} onSubmit={onSubmit} onDelete={onDelete} />
+            </DialogContent>
+          </Dialog>
+          <SectionDataList
+            sectionName={sectionName}
+            setSelectedListItem={setSelectedListItem}
+            setIsOpen={setIsOpen}
+          />
+          <FormNavButtons routeParam={sectionName} />
+        </>
+      ) : (
+        <FormGenerator {...sectionProps} values={getValues()} onSubmit={onSubmit} />
+      )}
+    </FormStepCard>
+  )
+}
--- form-generator/form-generator.tsx
+++ form-generator/form-generator.tsx
@@ -1,51 +1,47 @@
-"use client"
-import type { Employment, Language, School, Section } from "@/lib/stores/cv-data-store.types"
-import type { RouteParamType, SectionProps } from "./generator-sections"
+import type { SectionName } from "@/lib/stores/cv-data-store.types"
+import type { SectionProps } from "./generator-sections"
+import type { z } from "zod"
 
-import { Button } from "@/components/ui/button"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import { renderFieldComponent } from "@/form-generator/render-field"
-import { useFormNavigation } from "@/hooks/use-form-navigation"
-import { getDefaultValues } from "@/lib/generator-utils"
-import { useCvDataStore } from "@/components/providers/cv-data-store-provider"
+import FieldFactoryWrapper from "./field-factory-wrapper"
+import { sectionSchemas } from "./validation-schemas"
+import { Button } from "@/components/ui/button"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useState } from "react"
 import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { schemas } from "./validation-schemas"
+import { useFormNavigation } from "@/hooks/use-form-navigation"
 
-interface FormGeneratorProps {
-  fields: SectionProps["fields"]
-  routeParam: RouteParamType
-  isMultiEntry?: boolean
+interface FormGeneratorProps extends SectionProps {
+  values: z.infer<(typeof sectionSchemas)[SectionName]>
+  onSubmit: (data: z.infer<(typeof sectionSchemas)[SectionName]>) => void
+  onDelete?: () => void
 }
 
-export default function FormGenerator({ fields, routeParam, isMultiEntry }: FormGeneratorProps) {
-  const { setSectionData, addToList } = useCvDataStore((state) => state)
-  const { handleForwardStep, handleBackStep } = useFormNavigation(routeParam)
-  const sectionSchema = schemas[routeParam]
-  const form = useForm<z.infer<typeof sectionSchema>>({
-    resolver: zodResolver(sectionSchema),
-    defaultValues: getDefaultValues(fields),
-  })
+export default function FormGenerator({
+  fields,
+  sectionName,
+  isMultiEntry,
+  values,
+  onSubmit,
+  onDelete,
+}: FormGeneratorProps) {
+  const sectionSchema = sectionSchemas[sectionName]
+  const { handleBackStep } = useFormNavigation(sectionName)
+  const resolver = zodResolver(sectionSchema)
+  const form = useForm<z.infer<typeof sectionSchema>>({ resolver, values })
 
-  const [isCalendarOpen, setIsCalendarOpen] = useState<{ [key: string]: boolean }>({})
-
-  function onSubmit(data: Omit<z.infer<typeof sectionSchema>, "email">) {
+  function handleDelete() {
     if (isMultiEntry) {
-      addToList(routeParam as Section, data as Employment | School | Language)
-    } else {
-      setSectionData(routeParam as Section, data)
+      onDelete?.()
+      // const newData = data.filter((_, idx) => idx !== selectedListItem)
+      // setSectionData(sectionName, newData)
     }
-
-    handleForwardStep()
   }
 
   return (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="grid w-full gap-8"
+        className="grid w-full grid-cols-2 gap-8"
         // Columns had to be defined via inline style because the grid-cols-2 class didn't work for some reason
         style={{ gridTemplateColumns: Object.keys(fields).length > 3 ? "repeat(2, minmax(0, 1fr))" : "1fr" }}
       >
@@ -54,37 +50,48 @@ export default function FormGenerator({ fields, routeParam, isMultiEntry }: Form
           <FormField
             key={key}
             control={form.control}
-            name={key as keyof z.infer<typeof sectionSchema>}
+            name={key as string}
             render={({ field }) => (
               <FormItem
                 className="relative"
                 // Textareas should be full width all the time
                 // Columns had to be defined via inline style because the col-span-full class didn't work either
-                style={{ gridColumn: value.type === "textarea" ? "1 / -1" : undefined }}
+                style={{
+                  gridColumn: value.type === "textarea" || value.type === "image" ? "1 / -1" : undefined,
+                }}
               >
                 <FormLabel className="text-foreground">{value.label}</FormLabel>
                 <FormControl>
-                  {renderFieldComponent({
-                    field,
-                    key,
-                    value,
-                    isCalendarOpen,
-                    setIsCalendarOpen,
-                    setError: form.setError,
-                  })}
+                  <FieldFactoryWrapper field={field} key={key} value={value} setError={form.setError} />
                 </FormControl>
                 <FormMessage className="absolute -bottom-5 animate-in fade-in-0" />
               </FormItem>
             )}
           />
         ))}
-        <div className="mt-12 flex justify-center gap-8 max-sm:mt-8" style={{ gridColumn: "1 / -1" }}>
-          <Button type="button" className="w-36" onClick={handleBackStep}>
-            Vissza
-          </Button>
-          <Button type="submit" className="w-36">
-            Tovább
-          </Button>
+        <div className="mt-8 flex justify-center gap-10" style={{ gridColumn: "1 / -1" }}>
+          {isMultiEntry ? (
+            <>
+              <Button type="submit">Mentés</Button>
+              <Button type="button" variant={"destructive"} onClick={handleDelete}>
+                Törlés
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button
+                type="button"
+                variant={"navPrev"}
+                className="group relative px-10"
+                onClick={handleBackStep}
+              >
+                Vissza
+              </Button>
+              <Button type="submit" variant={"navNext"} className="group relative px-10">
+                Tovább
+              </Button>
+            </>
+          )}
         </div>
       </form>
     </Form>
