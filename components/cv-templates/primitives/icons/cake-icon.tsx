e548a5cd9f17a8f6f7d4d9ab6c8a987ddc401e78
myrddral <thirdeye0@gmail.com>
2024-08-15 06:23:07 +0200

    fixed icon positioning

--- components/cv-templates/primitives/icons/cake-icon.tsx
+++ components/cv-templates/primitives/icons/cake-icon.tsx
@@ -4,7 +4,7 @@ import { SvgIcon } from "../svg-icon"
 
 export const CakeIcon = ({}: SvgIconProps) => {
   return (
-    <SvgIcon viewBox="1 3 21 22" strokeWidth={1.9} style={{ position: "relative", top: 0.5 }}>
+    <SvgIcon viewBox="1 3 21 22" strokeWidth={1.9} style={{ position: "relative", top: -1 }}>
       <Path d="M20 21v-8a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v8" />
       <Path d="M4 16s.5-1 2-1 2.5 2 4 2 2.5-2 4-2 2.5 2 4 2 2-1 2-1" />
       <Path d="M2 21h20" />
