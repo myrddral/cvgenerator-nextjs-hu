08d4a989b907c1cf7a3cfb59057451d71504b6a8
myrddral <thirdeye0@gmail.com>
2026-07-02 14:17:26 +0200

    refactored Stepper component | adjjusted styles for Card, GridBackground, Stepper text

--- components/grid-background.tsx
+++ components/grid-background.tsx
@@ -1,5 +1,5 @@
 export function GridBackground() {
   return (
-    <div className="fade-edges fixed left-0 top-0 -z-10 min-h-[100dvh] w-screen bg-[url('/grid_bg1.svg')] bg-cover bg-center bg-no-repeat" />
+    <div className="fade-edges fixed left-1/2 top-1/2 -z-10 h-[100dvh] max-h-[1080px] w-[100dvw] max-w-[1920px] -translate-x-1/2 -translate-y-1/2 bg-[url('/grid_bg1.svg')] bg-cover bg-center bg-no-repeat" />
   )
 }
