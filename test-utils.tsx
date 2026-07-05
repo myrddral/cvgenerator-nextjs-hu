cbddaa09bb500db6cbfb6c432d0f647f562467ac
Attila Béli <thirdeye0@gmail.com>
2026-07-05 15:10:50 +0200

    feat: wrap custom render function in NextIntlClientProvider for locale support

--- test-utils.tsx
+++ test-utils.tsx
@@ -1,9 +1,14 @@
 import React, { type ReactElement } from "react"
 import { render, type RenderOptions } from "@testing-library/react"
+import { NextIntlClientProvider } from "next-intl"
 import { CvDataStoreProvider } from "./providers/cv-data-store-provider"
 
 const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
-  return <CvDataStoreProvider>{children}</CvDataStoreProvider>
+  return (
+    <NextIntlClientProvider locale="en" messages={{}}>
+      <CvDataStoreProvider>{children}</CvDataStoreProvider>
+    </NextIntlClientProvider>
+  )
 }
 
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions, "wrapper">) =>
